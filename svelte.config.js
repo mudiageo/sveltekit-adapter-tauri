d71de19f74390d19d08db339c9541260ebc539ce
Mudiaga Arharhire <mudiageo@gmail.com>
2024-10-31 14:43:59 +0100

    Manually point to adapter in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'sveltekit-adapter-tauri';
+import adapter from './node_modules/sveltekit-adapter-tauri/dist/index.js';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
